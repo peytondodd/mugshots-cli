a961cc468726b715bb3e9e3515131ab37cce7ddd
zacharytyhacz <zacharytepic@gmail.com>
2019-07-30 01:24:58 -0400

    Added route for total and renamed /info to /latest

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 import main
 from flask import Flask
 from flask import send_file
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/mugshots/info/<amount>")
+@app.route("/mugshots/latest/<amount>")
 def get_mugshots(amount):
-    return str(main.get_mugshots(int(amount)))
+    return jsonify(main.get_mugshots(int(amount)))
+
+@app.route("/mugshots/total")
+def get_total():
+    count_file = open("./count.txt","r")
+    count = int(count_file.readlines()[0]) + 1
+    count_file.close()
+    return str(count)
+
 
 @app.route("/mugshots/image/<id>")
 def get_image(id):
